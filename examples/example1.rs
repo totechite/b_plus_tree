71418eaaf979e4dbcfc2da8b8562af8617d3e702
yusuke kataoka <yusuke.kataoka09@gmail.com>
2021-02-23 16:30:31 +0900

    [fix] simplify

--- examples/example1.rs
+++ examples/example1.rs
@@ -3,37 +3,27 @@ extern crate b_plus_tree;
 
 use b_plus_tree::BPlusTree;
 use rand::Rng;
-
-use std::collections::BTreeMap;
-
 const VOLUME: usize = 100000;
 
 fn main() {
     let mut b_plus_tree = BPlusTree::new();
-    let mut b_tree = BTreeMap::new();
-    let test_data = gen_test_items();
-
-    let mut duplex = vec![];
+    let mut b_tree = std::collections::BTreeMap::new();
+    let test_data = gen_test_items(); // Randamy Numeric List
 
     for key in test_data {
         let data = format!("data: {:?}", key);
-        let v = b_plus_tree.insert(key, data.clone());
-        if let Some(_) = v {
-            duplex.push(key);
-        }
+        b_plus_tree.insert(key, data.clone());
         b_tree.insert(key, data);
     }
-    println!("{:?}", duplex);
-    for key in duplex {
-        let v = b_plus_tree.get(&key);
-        println!("{:?}", v);
-    }
-    println!("btree.len(): {:#?}", b_tree.len());
-    println!("{:?}", b_tree.range(100000..1000000));
 
-    println!("b_plus_tree.len(): {:#?}", b_plus_tree.len());
+    // Ordered entries
+    assert!(b_plus_tree.iter().is_sorted());
+
+    // Same contents as Btree
+    assert_eq!(b_tree.len(), b_plus_tree.len());
 }
 
+// Generate Numeric List
 fn gen_test_items() -> Vec<u32> {
     let mut rng = rand::thread_rng();
     let mut insert_items = Vec::with_capacity(VOLUME);
